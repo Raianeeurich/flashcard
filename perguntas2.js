17799ce57e4e3d5b05b85b7c10a2c234c5081c13
Raianeeurich <mittlstaedt.raiane@escola.pr.gov.br>
2025-08-18 11:52:44 -0300

    .

---
+++ perguntas2.js
@@ -0,0 +1,22 @@
+criaCartao(
+'Tecnologia',
+'Quem inventou a lâmpada?',
+' Thomas Edison.'
+)
+
+criaCartao(
+'Geografia',
+'Qual a cidade que está prestes a afundar?',
+'A cidade é Veneza'
+)
+
+criaCartao(
+'Ciências',
+'Quantos ossos temos no nosso corpo?',
+'206 ossos'
+)
+criaCartao(
+'Geografia',
+'Em que país se localiza o Taj Mahal?',
+'Ìndia'
+)
